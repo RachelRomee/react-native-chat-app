098f9d7060c5984a8e63c82ad84703910e71e626
RachelRomee <rachelromee@icloud.com>
2017-04-19 12:34:05 +0200

    fix error with missing feathers package

--- screens/SignUp.js
+++ screens/SignUp.js
@@ -7,6 +7,7 @@ import {
 import t from 'tcomb-form-native';
 import Person, { formOptions } from '../models/Person';
 import styles from './SignUp.styles';
+import signUp from '../actions/users/sign-up';
 
 export default class SignUp extends Component {
   constructor(props) {
@@ -36,6 +37,7 @@ export default class SignUp extends Component {
     const newUser = form.getValue();
     if (!newUser) return;
     console.log(newUser);
+	signUp(newUser);
 	this.clearForm();
   }
 
